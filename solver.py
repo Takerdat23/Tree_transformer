e57edd91cef45dec70df917c742ec229bfa381a3
TIENDAT23-LAPTO\Tien Dat <nguyentiendat9904@gmail.com>
2024-03-30 10:16:40 +0700

    add evaluate

--- solver.py
+++ solver.py
@@ -314,14 +314,14 @@ class Solver():
                 epoch_progress.update(1)
                 epoch_progress.set_postfix({'Loss': loss.item()})
 
-                if (step + 1) % 10 == 0:
+                if (step + 1) % 100 == 0:
                     elapsed = time.time() - start
                     print(f'Epoch [{epoch + 1}/{self.args.epoch}], Step [{step + 1}/{len(self.data_util.train_loader)}], '
                         f'Loss: {loss.item():.4f}, Total Time: {elapsed:.2f} sec')
-                    topic , sentiment = self.evaluate()
+                    # topic , sentiment = self.evaluate()
                
-                    print(f"Epoch {epoch} Validation accuracy (Aspect): ", topic)
-                    print(f"Epoch {epoch} Validation accuracy (Sentiment): ", sentiment)
+                    # print(f"Epoch {epoch} Validation accuracy (Aspect): ", topic)
+                    # print(f"Epoch {epoch} Validation accuracy (Sentiment): ", sentiment)
             epoch_progress.close()
             #Valid stage 
             topic , sentiment = self.evaluate()
