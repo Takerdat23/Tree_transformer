c35f568e1aa91b73dca27b275909ba460190b22f
TIENDAT23-LAPTO\Tien Dat <nguyentiendat9904@gmail.com>
2024-03-30 17:36:26 +0700

    fixed tokenizer

--- utils.py
+++ utils.py
@@ -235,7 +235,7 @@ class data_utils():
             
             tokenizer = ByteLevelBPETokenizer()
 
-            tokenizer.train_from_iterator(df_train["comment"], vocab_size=30000, min_frequency=2,
+            tokenizer.train_from_iterator(df_train["Review"], vocab_size=30000, min_frequency=2,
                               special_tokens=["[UNK]", "[CLS]", "[SEP]", "[PAD]", "[MASK]"])
             tokenizer.save_model(args.model_dir)
             self.tokenizer = ByteLevelBPETokenizer.from_file( os.path.join(args.model_dir,"vocab.json" ), os.path.join(args.model_dir,"merges.txt" ))
